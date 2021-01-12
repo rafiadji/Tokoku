449330b741fa1ecebb7a400e47749801f184c7d3
RAFI PRATAMA ADJI <rafi@stiki.ac.id>
2021-01-12 12:35:34 +0700

    tambah component

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "db_hrd";
+$database = "db_tokoku";
 
 $koneksi = mysqli_connect($server,$username,$password,$database) or die ("Koneksi Gagal");
 ?>
