0f482fc9b0d8ebbb3cf1f592a67fb1c4254a10c5
RAFI PRATAMA ADJI <rafi@stiki.ac.id>
2021-01-12 12:16:37 +0700

    pertama

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "db_hrd";
+
+$koneksi = mysqli_connect($server,$username,$password,$database) or die ("Koneksi Gagal");
+?>
